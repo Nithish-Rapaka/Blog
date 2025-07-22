cefc7a191f8633c1e7e9ff4f9f97bc8cd54af9a5
Nithish-Rapaka <rapakanithish83@gmail.com>
2025-07-22 07:33:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -277,6 +277,7 @@ app.delete('/theory/:subjectId/links/:linkIndex', async (req, res) => {
 
 
 // Start server
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("🚀 Server started on port 3000");
 });
